1c971ad3cd60a2acb142e0c3f287693f3223e9cb
Yinglian Liu <yinglianliu0217@gmail.com>
2022-04-11 11:52:35 -0400

    Get a new pattern

--- Short Study/SS7_LEDs/SS7_LEDs.ino
+++ Short Study/SS7_LEDs/SS7_LEDs.ino
@@ -1,16 +1,12 @@
 const int buttonPin = 2;
-
 int ledPins[ ]= {6,7,8,9,10,11,12,13};
-
 int buttonState = 0;
 
 void setup() {
   pinMode(buttonPin, INPUT);
   
   for (int i = 0; i <=7; i++) {
-    
     pinMode(ledPins[i],OUTPUT);
-    
     }
 }
 
@@ -27,30 +23,39 @@ void loop() {
 }
 
 void ledsLightUp(){
-  int delayTime = 100;
-  int delayTime2 = 300;
+  int delayTime = 100; 
+  int delayTime2 = 600;
 
-  for(int i =0; i<=7;i++)
+  //light up blue leds
+  for(int i =0; i<=7;i+=2)
   {
     digitalWrite(ledPins[i],HIGH);
     delay(delayTime);
   }
+  delay(delayTime2);
 
+  //light up whole
   for(int i =0; i<=7;i++)
   {
     digitalWrite(ledPins[i],HIGH);
   }
   
   delay(delayTime2);
-
-  for(int i =7; i>=0;i--)
+  
+  //turn off white leds
+  for(int i =7; i>=0;i-=2)
   {
     digitalWrite(ledPins[i],LOW);
     delay(delayTime);
-  }
-  
+  } 
     delay(delayTime2);
-  
+    
+    //turn off whole
+    for(int i =0; i<=7;i++)
+  {
+    digitalWrite(ledPins[i],LOW);
+  }
+  delay(delayTime2);
 }
 
 void ledsOff(){
